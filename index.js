0bdf98b7a76039660fb64d789814874a03a8afb4
PereiraJusto <rafa.spereira@icloud.com>
2025-10-03 16:22:42 -0300

    projeto1

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-let nome = "Rafael";
-let xp = 10001;
+let nome = "guilherme";
+let xp = 1001;
 
-switch (xp) {
-    case <=1000:
-        xp = "Ferro";
-        break;
-    case =>1001 && <=2000:
-        xp = "Bronze";
-        break;
-    case =>2001 && <=5000:
-        xp = "Prata"; 
-        break;
-    case =>5001 && <=7000:
-        xp = "Ouro";
-        break;
-    case =>7001 && <=8000:
-        xp = "Platina";
-        break;
-    case =>8001 && <=9000:
-        xp = "Ascendente";
-        break;
-    case =>9001 && <=10000:
-        xp = "Imortal";
-        break;
-    default:
-        xp = "Radiante";
-        break;
+if (xp <= 1000) {
+    xp = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    xp = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    xp = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+    xp = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+    xp = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    xp = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+    xp = "Imortal";
+} else {
+    xp = "Radiante";
 }
 console.log("O Herói de nome " + nome + " está no nível de " + xp + ".")
