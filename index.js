b0e91e7b11ded787edbf0f172b51c0cd8e435916
PereiraJusto <rafa.spereira@icloud.com>
2025-10-03 16:12:17 -0300

    desafio 1

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Oi Mundo!")
+let nome = "Rafael";
+let xp = 10001;
+
+switch (xp) {
+    case <=1000:
+        xp = "Ferro";
+        break;
+    case =>1001 && <=2000:
+        xp = "Bronze";
+        break;
+    case =>2001 && <=5000:
+        xp = "Prata"; 
+        break;
+    case =>5001 && <=7000:
+        xp = "Ouro";
+        break;
+    case =>7001 && <=8000:
+        xp = "Platina";
+        break;
+    case =>8001 && <=9000:
+        xp = "Ascendente";
+        break;
+    case =>9001 && <=10000:
+        xp = "Imortal";
+        break;
+    default:
+        xp = "Radiante";
+        break;
+}
+console.log("O Herói de nome " + nome + " está no nível de " + xp + ".")
